1ecd0d3e3934a5319aaadfac2055f828fbc21f62
Tessa Power <tessa@tessapower.co>
2022-12-18 23:06:21 +1300

    Rename ImageLoader to TImageLoader

--- tengine/src/main/java/tengine/graphics/entities/sprites/TImageLoader.java
+++ tengine/src/main/java/tengine/graphics/entities/sprites/TImageLoader.java
@@ -5,7 +5,15 @@ import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
 
-class ImageLoader {
+/**
+ * A helper class used to load images.
+ *
+ * @author Tessa Power
+ */
+class TImageLoader {
+    /**
+     * Loads an <code>Image</code> using the given <code>InputStream</code>.
+     */
     static Image loadImage(InputStream is) {
         try {
             return ImageIO.read(is);
