ee91229dbccdb8e6ff30559de975006f6f98e310
Tessa Power <tessa@tessapower.co>
2022-12-18 22:20:58 +1300

    Refactor and document buttons and buttongroup

--- snake/src/main/java/snake/ui/components/Button.java
+++ snake/src/main/java/snake/ui/components/Button.java
@@ -4,17 +4,30 @@ import snake.assets.Colors;
 import snake.assets.FontBook;
 import tengine.graphics.entities.text.TLabel;
 
+/**
+ * A simple text button that can have a focussed or unfocused state.
+ */
 public class Button extends TLabel {
+    /**
+     * The two possible states that a <code>Button</code> can have, focussed or unfocused.
+     */
     public enum State {
         FOCUSSED, UNFOCUSED
     }
 
+    /**
+     * Constructs a new <code>Button</code> with the given string. The initial <code>State</code>
+     * is unfocused.
+     */
     public Button(String text) {
         super(text);
         setState(State.UNFOCUSED);
         setFont(FontBook.shared().buttonFont());
     }
 
+    /**
+     * Set the <code>State</code> of this <code>Button</code>.
+     */
     public void setState(State state) {
         setColor(
             switch(state) {
--- snake/src/main/java/snake/ui/components/ButtonGroup.java
+++ snake/src/main/java/snake/ui/components/ButtonGroup.java
@@ -7,13 +7,23 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * This is a quick and dirty "radio-group-like" management for buttons, where only a single
- * button can be focussed at a time. This suffices for all the use cases in snek.
+ * A quick and dirty "radio-group-like" management for buttons, where only a single button can be
+ * focussed at a time. This suffices for all the use cases in snek.
+ *
+ * @author Tessa Power
+ * @see Button
  */
 public class ButtonGroup {
     private final List<Button> buttons;
     private int focusIdx;
 
+    /**
+     * Create a new <code>ButtonGroup</code> containing the given <code>Button</code>s. Buttons
+     * will be navigated in the same order they are passed in, so their physical position on the
+     * screen should ideally match this ordering. The first button will be automatically set as
+     * the focussed button, change this by calling <code>setFocussed</code> on the
+     * <code>ButtonGroup</code>.
+     */
     public ButtonGroup(Button... buttons) {
         this.buttons = new ArrayList<>(buttons.length);
         this.buttons.addAll(Arrays.asList(buttons));
@@ -21,6 +31,10 @@ public class ButtonGroup {
         focusIdx = 0;
     }
 
+    /**
+     * Focus the next <code>Button</code> in this <code>ButtonGroup</code>. Stops when it
+     * reaches the end, i.e. does not wrap around to the beginning.
+     */
     public void next() {
         if (focusIdx < buttons.size() - 1) {
             SoundEffects.shared().menuMove().play();
@@ -30,6 +44,10 @@ public class ButtonGroup {
         }
     }
 
+    /**
+     * Focus the previous <code>Button</code> in this <code>ButtonGroup</code>. Stops when it
+     * reaches the beginning, i.e. does not wrap around to the end.
+     */
     public void previous() {
         if (focusIdx > 0) {
             SoundEffects.shared().menuMove().play();
@@ -39,16 +57,25 @@ public class ButtonGroup {
         }
     }
 
+    /**
+     * Returns the currently focussed <code>Button</code> in this <code>ButtonGroup</code>.
+     */
     public Button getFocussed() {
         return buttons.get(focusIdx);
     }
 
+    /**
+     * Set the currently focussed <code>Button</code> in this <code>ButtonGroup</code>. Does
+     * nothing if the given <code>Button</code> doesn't belong to this <code>ButtonGroup</code>.
+     */
     public void setFocussed(Button button) {
-        for (var b : buttons) {
-            if (b.equals(button)) {
-                b.setState(Button.State.FOCUSSED);
-            } else {
-                b.setState(Button.State.UNFOCUSED);
+        if (buttons.contains(button)) {
+            for (var b : buttons) {
+                if (b.equals(button)) {
+                    b.setState(Button.State.FOCUSSED);
+                } else {
+                    b.setState(Button.State.UNFOCUSED);
+                }
             }
         }
     }
