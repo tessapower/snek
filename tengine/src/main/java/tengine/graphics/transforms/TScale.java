007de8692162555cca7e117050a308f221db0085
Tessa Power <tessa@tessapower.co>
2022-12-18 23:04:18 +1300

    Add comments to TScale and TTranslation

--- tengine/src/main/java/tengine/graphics/transforms/TScale.java
+++ tengine/src/main/java/tengine/graphics/transforms/TScale.java
@@ -2,19 +2,35 @@ package tengine.graphics.transforms;
 
 import java.util.Objects;
 
+/**
+ * Represents a scaling transform.
+ *
+ * @author Tessa Power
+ * @see TTranslation
+ * @see TRotation
+ */
 public class TScale {
     public double xScaleFactor;
     public double yScaleFactor;
 
+    /**
+     * Constructs a scaling transform.
+     */
     public TScale(double xScaleFactor, double yScaleFactor) {
         this.xScaleFactor = xScaleFactor;
         this.yScaleFactor = yScaleFactor;
     }
 
+    /**
+     * Represents the identity scaling of 1.
+     */
     public static TScale identity() {
         return new TScale(1, 1);
     }
 
+    /**
+     * Compares two <code>TScale</code>s to see if they are equal.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- tengine/src/main/java/tengine/graphics/transforms/TTranslation.java
+++ tengine/src/main/java/tengine/graphics/transforms/TTranslation.java
@@ -2,19 +2,35 @@ package tengine.graphics.transforms;
 
 import java.util.Objects;
 
+/**
+ * Represents a translation transform as a delta from the current origin (dx, dy).
+ *
+ * @author Tessa Power
+ * @see TRotation
+ * @see TScale
+ */
 public class TTranslation {
     public int dx;
     public int dy;
 
+    /**
+     * Constructs a new <code>TTranslation</code> in the form (dx, dy).
+     */
     public TTranslation(int dx, int dy) {
         this.dx = dx;
         this.dy = dy;
     }
 
+    /**
+     * Represents the identity translation of (0, 0).
+     */
     public static TTranslation identity() {
         return new TTranslation(0, 0);
     }
 
+    /**
+     * Compares two <code>TTranslations</code> to see if they are equal.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
